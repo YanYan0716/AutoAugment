e2223ff3f4b015d1ad6e5e062d7737c982e51a63
yanqian <yanqian0716@gmail.com>
2021-03-01 11:08:50 +0800

    .

--- WideResnet.py
+++ WideResnet.py
@@ -23,12 +23,12 @@ class BasicBlock(layers.Layer):
         self.trainable = trainable
 
         self.bn1 = layers.BatchNormalization(
-            # momentum=0.001,
+            momentum=0.001,
             trainable=self.trainable,
             name=name+'_bn1'
         )
-        # self.relu1 = layers.LeakyReLU(alpha=0.1)
-        self.relu1 = keras.activations.relu
+        self.relu1 = layers.LeakyReLU(alpha=0.2)
+        # self.relu1 = keras.activations.relu
         self.conv1 = layers.Conv2D(
             filters=self.out_channels,
             kernel_size=3,
@@ -41,11 +41,11 @@ class BasicBlock(layers.Layer):
             name=name+'_conv1',
         )
         self.bn2 = layers.BatchNormalization(
-            # momentum=0.001,
+            momentum=0.001,
             trainable=self.trainable,
             name=name+'_bn2'
         )
-        self.relu2 = keras.activations.relu #layers.LeakyReLU(alpha=0.1)
+        self.relu2 = layers.LeakyReLU(alpha=0.2)
         self.drop_layer = layers.Dropout(
             rate=self.dropout,
             trainable=self.trainable,
@@ -76,7 +76,7 @@ class BasicBlock(layers.Layer):
             )
         self.add = layers.Add(name=name+'_add')
 
-    def call(self, inputs, **kwargs):
+    def call(self, inputs):
         out = self.bn1(inputs)
         out = self.relu1(out)
         out = self.conv1(out)
@@ -126,11 +126,12 @@ class WideResnet(keras.Model):
         self.Basic12 = BasicBlock(in_channels=k[3], out_channels=k[3], stride=1, dropout=self.dropout, name=name+'_Basic12', trainable=True)
 
         self.bn1 = layers.BatchNormalization(
+            momentum=0.001,
             trainable=self.trainable,
             name=name+'_bn1'
         )
-        # self.relu1 = layers.LeakyReLU(alpha=0.1)
-        self.relu1 = keras.activations.relu
+        self.relu1 = layers.LeakyReLU(alpha=0.2)
+        # self.relu1 = keras.activations.relu
         self.avgpool = layers.GlobalAveragePooling2D(name=name+'_avgpool')
         self.dense = layers.Dense(
             units=config.NUM_CLASS,
